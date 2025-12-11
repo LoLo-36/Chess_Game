c43dcfaae747ce9842c4eb284662dbff412ec8d2
Long <lelong28032006@icloud.com>
2025-12-11 23:33:03 +0700

    docs: add javadoc to Piece.java

--- src/main/java/org/example/entities/Piece.java
+++ src/main/java/org/example/entities/Piece.java
@@ -2,6 +2,13 @@ package org.example.entities;
 
 import java.awt.*;
 
+/**
+ * This class initializes the Piece object,
+ * And defines operations on the piece.
+ *     get symbol of piece,
+ *     check to see if the position of this piece coincides with another piece,
+ *     Check if this piece can move to the specified square
+ */
 public abstract class Piece {
     private int coordinatesX;
     private int coordinatesY;
@@ -64,12 +71,31 @@ public abstract class Piece {
         this.firstMove = firstMove;
     }
 
+    /**
+     * Check to see if the position of this piece coincides with another piece.
+     *
+     * @param piece the another piece
+     * @return true if this piece coincides with another piece
+     */
     public boolean checkPosition(Piece piece) {
         return this.coordinatesX == piece.getCoordinatesX()
                 && this.coordinatesY == piece.getCoordinatesY();
     }
 
+    /**
+     * Get symbol of piece.
+     *
+     * @return symbol of piece as a String
+     */
     public abstract String getSymbol();
 
+    /**
+     *  Check if this piece can move to the specified square.
+     *
+     * @param board The board contains piece.
+     * @param x coordinates X of square
+     * @param y coordinates Y of square
+     * @return true if the piece can move to that square (x, y)
+     */
     public abstract boolean canMove(Board board, int x, int y);
 }

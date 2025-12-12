023f9f117819f290326f2fa2ece3b3ae26d8ba5e
Long <lelong28032006@icloud.com>
2025-12-12 12:15:27 +0700

    docs: add javadoc to User.java

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -1,9 +1,20 @@
 package org.example.user;
 
+/**
+ * Represents a user in the system with a unique identifier and a username.
+ * This class provides basic information about a user and allows modifying
+ * the user's ID and username after creation.
+ */
 public class User {
     private String id;
     private String username;
 
+    /**
+     * Creates a new User with the given id and username.
+     *
+     * @param id        the unique identifier of the user
+     * @param username  the username of the user
+     */
     public User(String id, String username) {
         this.id = id;
         this.username = username;
